d4c04eb0b918d0fb5998988d1cef13d48506d038
mambax7 <mambax7@gmail.com>
2015-06-06 05:33:40 -0400

    Original files

---
+++ old_brown/manifesto.ini.php
@@ -0,0 +1,20 @@
+<?php
+/**
+
+[Manifesto]
+Name="old_brown"
+Depends=Legacy_RenderSystem
+Url="http://www.expiece.com/html/"
+Version="1.0"
+
+[Theme]
+RenderSystem=Legacy_RenderSystem
+Format="XOOPS2 Legacy Style"
+Author=hi-debow @ EXTRAFFIC
+ScreenShot="screenshot.png"
+Description="2 columns fixed width 900px tableless CSS layout style theme"
+
+License="Attribution-Noncommercial 2.1 Japan"
+
+*/
+?>
